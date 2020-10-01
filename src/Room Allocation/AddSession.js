cd5b4602929dfd311d9c156390d3b57ebfb0da27
venuriQL <venuri@quantalab.tech>
2020-10-01 08:58:24 +0530

    room allocation added

---
+++ src/Room Allocation/AddSession.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+
+export default class AddSession extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>
+                thiss is addsession
+            </div>
+        );
+    }
+}
---
+++ src/Room Allocation/UnavailableRooms.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+
+export default class UnavailableRooms extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>
+                thiss is unavailable
+            </div>
+        );
+    }
+}
