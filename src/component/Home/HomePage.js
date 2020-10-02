61fd6d7d810ebe452d7ef3ee0023b1f81c296e31
DulminiD <dulminid1107@gmail.com>
2020-10-02 22:31:01 +0530

    Final edit

--- src/component/Home/HomePage.js
+++ src/component/Home/HomePage.js
@@ -5,35 +5,44 @@ class HomePage extends React.Component{
     render() {
         return(
             <div>
-                <div className='container-fluid' >
-                    <Carousel>
-                        <Carousel.Item style={{'height':"450px"}} >
-                            <img style={{'height':"500px"}}
-                            className="d-block w-100"
-                            src={'../../assets/img/12.jpg'}  />
-                        <Carousel.Caption>
-                            <h3>University Time-Table Generator</h3>
-                        </Carousel.Caption>
-                    </Carousel.Item  >
-                    <Carousel.Item style={{'height':"450px"}}>
-                        <img style={{'height':"500px"}}
-                        className="d-block w-100"
-                        src={'../../assets/img/13.jpg'}    />
-                    <Carousel.Caption>
-                        <h3>The challenge is not to manage time, but to manage ourselves</h3>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item style={{'height':"450px"}}>
-                    <img style={{'height':"500px"}}
-                    className="d-block w-100"
-                    src={'../../assets/img/14.jpg'}   />
-                <Carousel.Caption>
-                    <h3>WE-27</h3>
-                </Carousel.Caption>
-            </Carousel.Item>
-    </Carousel>
-    </div>
-    </div>
+            <div style={{width:'850px',height:"500px",marginTop:'15px', 'background-color': '#ccff99', "border-color":"#000000"}} >
+
+                <div>
+                    <h4 style={{marginLeft:'35px', padding:'30px', marginTop:'15px'}}>The challenge is not to manage time, but to manage ourselves</h4>
+                    <h2 style={{marginLeft:'20%', padding:'30px', marginTop:'35px'}}>University Time-Table Generator</h2>
+                    <h5 style={{marginLeft:'40%', padding:'30px', marginTop:'15px'}}>WE-27</h5>
+                </div>
+    {/*            <div className='container-fluid' >*/}
+    {/*                <Carousel>*/}
+    {/*                    <Carousel.Item style={{'height':"450px"}} >*/}
+    {/*                        <img style={{'height':"500px"}}*/}
+    {/*                        className="d-block w-100"*/}
+    {/*                        src={require('../../assets/12.jpg')}  />*/}
+    {/*                    <Carousel.Caption>*/}
+    {/*                        <h3>University Time-Table Generator</h3>*/}
+    {/*                    </Carousel.Caption>*/}
+    {/*                </Carousel.Item  >*/}
+    {/*                <Carousel.Item style={{'height':"450px"}}>*/}
+    {/*                    <img style={{'height':"500px"}}*/}
+    {/*                    className="d-block w-100"*/}
+    {/*                    src={require('../../assets/13.jpg')}    />*/}
+    {/*                <Carousel.Caption>*/}
+    {/*                    <h3>The challenge is not to manage time, but to manage ourselves</h3>*/}
+    {/*                </Carousel.Caption>*/}
+    {/*            </Carousel.Item>*/}
+    {/*            <Carousel.Item style={{'height':"450px"}}>*/}
+    {/*                <img src={require('../../14.jpg')}*/}
+    {/*                     style={{'height':"500px"}}*/}
+    {/*                     className="d-block w-100"*/}
+    {/*                 />*/}
+    {/*            <Carousel.Caption>*/}
+    {/*                <h3>WE-27</h3>*/}
+    {/*            </Carousel.Caption>*/}
+    {/*        </Carousel.Item>*/}
+    {/*</Carousel>*/}
+    {/*</div>*/}
+            </div>
+            </div>
     )
     }
 }
